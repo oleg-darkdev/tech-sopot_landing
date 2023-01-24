af15115f2eb39928ced758048fccb853ad5662c5
DarkDev <darkdev_brand@riseup.net>
2023-01-24 08:48:09 +0200

    add registration link value

--- src/data/workshopsList.js
+++ src/data/workshopsList.js
@@ -3,122 +3,123 @@
 			id: 0,
 			title: 'Tworzenie edukacyjnych gier planszowych',
 			goal: 'Wprowadzenie młodych ludzi w świat robienija gier planszowych',
-		 descr: [
-			 'Własnoręczne tworzenie edukacyjnych gier planszowych - od pomysłu do realizacji.',
-			 '',
-			 ''
-		 ],
+			descr: [
+				'Własnoręczne tworzenie edukacyjnych gier planszowych - od pomysłu do realizacji.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 1,
 			title: 'Sieci neuronowe, sztuczna inteligencja ',
 			goal: 'Wprowadzenie młodych ludzi w świat IT do dziedziny sieci neuronowych i sztucznej inteligencji',
-			descr:
-				[
-					'Ogólne informacje techniczne o tym, jak powstają sieci neuronowe, jakie są ich typy i jak się kształtują',
-					'',
-					''
-				],
+			descr: [
+				'Ogólne informacje techniczne o tym, jak powstają sieci neuronowe, jakie są ich typy i jak się kształtują',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 30
+			maxVisitors: 30,
+			registration: ''
 		},
 
 		{
 			id: 2,
 			title: 'Sieci neuronowe, sztuczna inteligencja',
 			goal: 'wprowadzenie młodych ludzi w świat IT do dziedziny sieci neuronowych i sztucznej inteligencji',
-			descr:
-				[
-					'Ogólne informacje techniczne o tym, jak powstają sieci neuronowe, jakie są ich typy i jak się kształtują.',
-					'',
-					''
-				],
+			descr: [
+				'Ogólne informacje techniczne o tym, jak powstają sieci neuronowe, jakie są ich typy i jak się kształtują.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 30
+			maxVisitors: 30,
+			registration: ''
 		},
 		{
 			id: 3,
 			title:
 				'Analiza działania sztucznej inteligencji w kontekście tworzenia sensownego tekstu, praktyka',
 			goal: 'Wprowadzenie młodych ludzi w świat IT',
-			descr:
-				[
-					'Teoretyczne i praktyczne zgłębienie sieci neuronowych wchodzących w interakcję z człowiekiem za pośrednictwem tekstu, na przykładzie OpenAi / chatGPT.',
-					'',
-					''
-				],
+			descr: [
+				'Teoretyczne i praktyczne zgłębienie sieci neuronowych wchodzących w interakcję z człowiekiem za pośrednictwem tekstu, na przykładzie OpenAi / chatGPT.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 4,
 			title:
 				'Analiza działania sztucznej inteligencji do tworzenia materiałów graficznych, praktyka',
 			goal: 'Wprowadzenie młodych ludzi w świat IT',
-			descr:
-				[
-					'Teoretyczne i praktyczne studium sieci neuronowych generujących obrazy na podstawie tekstowego żądania człowieka na przykładzie Midjourney, OpenAi / DALL·E 2.',
-					'',
-					''
-				],
+			descr: [
+				'Teoretyczne i praktyczne studium sieci neuronowych generujących obrazy na podstawie tekstowego żądania człowieka na przykładzie Midjourney, OpenAi / DALL·E 2.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 30
+			maxVisitors: 30,
+			registration: ''
 		},
 		{
 			id: 5,
 			title: '3D druk: jak działa technologia, używane materiały, informacja ogólne',
 			goal: 'Wprowadzenie młodych ludzi w świat IT do dziedziny 3D druku i modelowania 3D.',
-			descr:
-				[
-					'Wprowadzenie, jak odbywa się druk 3D, jakie są rodzaje druku 3D.Analiza materiałów, z których drukowane są części 3D.Ogólne informacje o przemysłowych, profesjonalnych i konsumenckich drukarkach 3D.',
-					'',
-					''
-				],
+			descr: [
+				'Wprowadzenie, jak odbywa się druk 3D, jakie są rodzaje druku 3D.Analiza materiałów, z których drukowane są części 3D.Ogólne informacje o przemysłowych, profesjonalnych i konsumenckich drukarkach 3D.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 20
+			maxVisitors: 20,
+			registration: ''
 		},
 		{
 			id: 6,
 			title: '3D druk: oprogramowanie, praktyka',
 			goal: 'Praktyczne zapoznanie się z różnymi programami do druku 3D.',
-			descr:
-				[
-					'Praktyczne zastosowanie slicerów i oprogramowania do projektowania 3D. Lista programów, które będą poznawane na lekcji: Sliceru, Cura, Simplify3D, MakerBot Print, OctoPrint, Slic3r i oprogramowanie do modelowania 3D: Autodesk Fusion 360, TinkerCAD, Blender, Compass 3D',
-					'',
-					''
-				],
+			descr: [
+				'Praktyczne zastosowanie slicerów i oprogramowania do projektowania 3D. Lista programów, które będą poznawane na lekcji: Sliceru, Cura, Simplify3D, MakerBot Print, OctoPrint, Slic3r i oprogramowanie do modelowania 3D: Autodesk Fusion 360, TinkerCAD, Blender, Compass 3D',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 20
+			maxVisitors: 20,
+			registration: ''
 		},
 		{
 			id: 7,
@@ -134,42 +135,43 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 20
+			maxVisitors: 20,
+			registration: ''
 		},
 		{
 			id: 8,
 			title: 'Zawód fotografa i sztuczna inteligencja',
 			goal: 'Wprowadzenie do zawodu fotografa.',
-			descr:
-				[
-					'Wprowadzenie do zawodu fotografa. Gościnnie występuje fotograf: Monika Rucinska. Pokazanie uczestnikom, jak sieci neuronowe zmieniają zawód fotografa i fotoedytora.',
-					'',
-					''
-				],
+			descr: [
+				'Wprowadzenie do zawodu fotografa. Gościnnie występuje fotograf: Monika Rucinska. Pokazanie uczestnikom, jak sieci neuronowe zmieniają zawód fotografa i fotoedytora.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 20
+			maxVisitors: 20,
+			registration: ''
 		},
 		{
 			id: 9,
 			title:
 				'Katedra Kultury Białoruskiej - warsztat międzykulturowe: co wspólnego mają kultury Białorusi i Polski',
 			goal: 'Na konkretnych przykładach pokazać, co łączy kultury Polski i Białorusi.',
-			descr:
-				[
-					'Sesja składa się z wykładu wprowadzającego 25, oraz części praktycznej z wykorzystaniem edukacyjnej gry planszowej.',
-					'',
-					''
-				],
+			descr: [
+				'Sesja składa się z wykładu wprowadzającego 25, oraz części praktycznej z wykorzystaniem edukacyjnej gry planszowej.',
+				'',
+				''
+			],
 			addres: 'Dom Sąsiedzki "Tęczowy Dom" ul. 23 Marca 32c, Sopot',
 			duration: '180',
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 10,
@@ -184,7 +186,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 16
+			maxVisitors: 16,
+			registration: ''
 		},
 
 		{
@@ -197,7 +200,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 16
+			maxVisitors: 16,
+			registration: ''
 		},
 		{
 			id: 12,
@@ -209,7 +213,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 16
+			maxVisitors: 16,
+			registration: ''
 		},
 		{
 			id: 13,
@@ -221,7 +226,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 16
+			maxVisitors: 16,
+			registration: ''
 		},
 		{
 			id: 14,
@@ -233,7 +239,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 16
+			maxVisitors: 16,
+			registration: ''
 		},
 		{
 			id: 15,
@@ -249,7 +256,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 24
+			maxVisitors: 24,
+			registration: ''
 		},
 
 		{
@@ -266,7 +274,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 17,
@@ -282,7 +291,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 18,
@@ -298,7 +308,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 19,
@@ -314,7 +325,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 20,
@@ -330,7 +342,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 21,
@@ -342,7 +355,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 22,
@@ -358,7 +372,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 23,
@@ -374,7 +389,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		},
 		{
 			id: 24,
@@ -390,7 +406,8 @@
 			date: '28.12.2022',
 			startTime: '17:00',
 			endTime: '19:00',
-			maxVisitors: 12
+			maxVisitors: 12,
+			registration: ''
 		}
  ];
 
